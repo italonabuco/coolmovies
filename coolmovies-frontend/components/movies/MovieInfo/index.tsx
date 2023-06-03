db4115660b49f6988471f50c0c80a0dd7d1e18cc
Italo Nabuco <italonabuco@hotmail.com>
2023-06-02 23:52:26 -0300

    shows movie info on review page

---
+++ coolmovies-frontend/components/movies/MovieInfo/index.tsx
@@ -0,0 +1,57 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+
+interface IMovieInfoType {
+  img: string;
+  title: string;
+  year: string;
+}
+
+const MovieInfo: React.FC<IMovieInfoType> = ({ img, title, year }) => {
+  const classes = useStyles();
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <CardMedia
+        className={classes.img}
+        component='img'
+        image={img}
+        alt={title}
+      />
+      <Box
+        sx={{
+          p: 1,
+        }}
+      >
+        <Typography component='p' color='text.primary' variant='h5'>
+          {title}
+        </Typography>
+        <Typography component='p' color='text.secondary' variant='body1'>
+          {year}
+        </Typography>
+        <Button variant='outlined' color='warning' sx={{ marginTop: 2 }}>
+          Rate movie
+        </Button>
+      </Box>
+    </Card>
+  );
+};
+
+const useStyles = makeStyles((theme: any) => ({
+  img: {
+    width: 200,
+    [theme.breakpoints.down('sm')]: {
+      width: 120,
+    },
+  },
+}));
+
+export default MovieInfo;
--- coolmovies-frontend/pages/reviews/[id]/index.tsx
+++ coolmovies-frontend/pages/reviews/[id]/index.tsx
@@ -1,13 +1,31 @@
 import { NextPage } from 'next';
 import AppLayout from '../../../components/layouts/AppLayout';
 import { useRouter } from 'next/router';
+import { reviewsActions, useAppDispatch, useAppSelector } from '../../../redux';
+import { useEffect } from 'react';
+import MovieInfo from '../../../components/movies/MovieInfo';
 
 const Reviews: NextPage = () => {
   const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { reviews, movie, loading, error } = useAppSelector(
+    (state) => state.reviews
+  );
+
+  useEffect(() => {
+    if (router.query.id)
+      dispatch(reviewsActions.fetch({ id: router.query.id.toString() }));
+  }, [dispatch, router.query.id]);
 
   return (
-    <AppLayout title={`Reviews for movieId: ${router.query.id}`}>
-      List of reviews
+    <AppLayout title={'Reviews'}>
+      {movie && (
+        <MovieInfo
+          img={movie.imgUrl}
+          title={movie.title}
+          year={movie.releaseDate.slice(0, 'yyyy'.length)}
+        />
+      )}
     </AppLayout>
   );
 };
